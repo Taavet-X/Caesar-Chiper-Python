8f033545135a8ab45655243972f03111bf60e49e
David Estrada <77510617+Desth-X@users.noreply.github.com>
2022-07-22 16:14:38 -0500

    Update Method.py

--- Method.py
+++ Method.py
@@ -1,13 +1,38 @@
-chars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+chars = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+    'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+    'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ']
 
-word = "Test"
-word = word.upper()
-shift = 7
-newWord = ""
-for char in word:
-    charIndex = chars.index(char)
-    newIndex = charIndex + shift
-    newIndex = newIndex % len(chars)
-    newWord += chars[newIndex]
-print(newWord)
+def encode(text, shift):
+    text = text.upper() 
+    encodedText = ""
+    #for each character in the text
+    for char in text: 
+        #gets the position of the char in the list        
+        charIndex = chars.index(char) 
+        #then adds a value that that position
+        newIndex = charIndex + shift 
+        #applies mod to avoid going out of bounds
+        newIndex = newIndex % len(chars) 
+        #adds the new char to the result text
+        encodedText += chars[newIndex]
+    return encodedText
+
+def decode(encodedText, shift):
+    encodedText = encodedText.upper() #All letters into cap ones
+    decodedText = ""
+    #for each character in the text
+    for char in encodedText:
+        #gets the position of the char in the list
+        charIndex = chars.index(char)
+        #then adds a value that that position, this time
+        #the value is the diference between the amount 
+        #of chars in the list in the list and the shift
+        newIndex = charIndex + len(chars) - shift
+        newIndex = newIndex % len(chars)
+        decodedText += chars[newIndex]
+    return decodedText
+
+print(encode("Hello World", 7))
+print(decode("OLSSVGCVYSK", 7))
   
